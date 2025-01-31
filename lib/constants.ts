01ceeba4726b56258f6e8f18430415f355e662b3
lakshyaag <lakshyajannu@gmail.com>
2025-01-31 13:28:48 +0530

    intro update

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,6 +6,6 @@ export const LANDING_INTRO = `👋🏼 Welcome to my little corner on the Intern
 
 I'm currently working as AI Engineer, building LLM-based systems for the education industry. Previously, I spent time at Bain & Company, crunching numbers for private equity.
 
-Outside of work, I love playing video games, travelling and clicking pictures, and learning new things.
+Outside of work, you'll find me optimizing assembly lines in Factorio (after years of Counter-Strike), capturing landscapes across countries with my phone, or struggling to learn Mandarin - because, why not?
 
 If you have an interesting project that you want to build with me, or ideas to explore together, I’d love to [chat](mailto:${EMAIL}?subject=Hello%20from%20your%20website!)!`;
