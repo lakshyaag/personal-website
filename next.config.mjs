f207b284c216becfc6f01367cc6897c04aa42f55
lakshyaag <lakshyajannu@gmail.com>
2025-11-18 09:01:04 -0500

    fix webpack

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,19 @@ import createMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import rehypeHighlight from 'rehype-highlight';
-
+import rehypeHighlight from "rehype-highlight";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	turbopack: {},
 	images: {
-		remotePatterns: [new URL("https://books.google.com")],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "books.google.com",
+			},
+		],
 	},
 };
 
